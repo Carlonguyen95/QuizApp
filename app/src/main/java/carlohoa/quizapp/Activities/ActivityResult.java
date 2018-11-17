84a7ca5cf447d337fc7c95f093a1dd1c25639f98
Carlonguyen95 <s315613@stud.hioa.no>
2018-11-17 15:06:02 +0100

    init

---
+++ app/src/main/java/carlohoa/quizapp/Activities/ActivityResult.java
@@ -0,0 +1,15 @@
+package carlohoa.quizapp.Activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import carlohoa.quizapp.R;
+
+public class ActivityResult extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_on_finish);
+    }
+}
